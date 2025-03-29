6a3a42c19918bcb038a0a1a4f9718a3dad34ef7a
Miepee <janbidler00@protonmail.com>
2025-03-29 16:06:49 +0100

    Prime 1 support

--- randovania/games/prime1/gui/preset_settings/__init__.py
+++ randovania/games/prime1/gui/preset_settings/__init__.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override
+
+from randovania.gui.preset_settings.metroid_item_pool_tab import MetroidPresetItemPool
 
 if TYPE_CHECKING:
+    from randovania.game_description.pickup.pickup_database import PickupDatabase
     from randovania.gui.lib.window_manager import WindowManager
     from randovania.gui.preset_settings.preset_tab import PresetTab
     from randovania.interface_common.preset_editor import PresetEditor
+
+
+class Prime1PresetItemPool(MetroidPresetItemPool):
+    @override
+    def get_energy_tank_item(self, pickup_database: PickupDatabase):
+        return pickup_database.standard_pickups["Power Shoe"]
 
 
 def prime1_preset_tabs(editor: PresetEditor, window_manager: WindowManager) -> list[type[PresetTab]]:
@@ -18,7 +27,6 @@ def prime1_preset_tabs(editor: PresetEditor, window_manager: WindowManager) -> l
     from randovania.gui.preset_settings.dock_rando_tab import PresetDockRando
     from randovania.gui.preset_settings.hints_tab import PresetHints
     from randovania.gui.preset_settings.location_pool_tab import PresetLocationPool
-    from randovania.gui.preset_settings.metroid_item_pool_tab import MetroidPresetItemPool
     from randovania.gui.preset_settings.patcher_energy_tab import PresetPatcherEnergy
     from randovania.gui.preset_settings.starting_area_tab import PresetMetroidStartingArea
     from randovania.gui.preset_settings.trick_level_tab import PresetTrickLevel
@@ -29,7 +37,7 @@ def prime1_preset_tabs(editor: PresetEditor, window_manager: WindowManager) -> l
         PresetPrimeGoal,
         PresetHints,
         PresetLocationPool,
-        MetroidPresetItemPool,
+        Prime1PresetItemPool,
         PresetPatcherEnergy,
         PresetTeleportersPrime1,
         PresetMetroidStartingArea,
